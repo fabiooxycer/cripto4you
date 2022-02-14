db7fb605dd56d86c69ae939c6ec9d6a89344ed27
fabiooxycer <fabiovieira@oxycer.com>
2022-02-14 17:00:33 -0300

    Update index.php

--- broker/index.php
+++ broker/index.php
@@ -12,9 +12,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
     <?php include('includes/scripts.php'); ?>
-
 </head>
 
 <body class="bg-gradient-primary">
@@ -31,10 +29,10 @@
                                     </div>
                                     <form class="user" action="senha" method="POST">
                                         <div class="form-group">
-                                            <input type="email" class="form-control" id="email" name="email" onChange="this.value=this.value.toLowerCase()" placeholder="INFORME SEU E-MAIL" required>
+                                            <input type="email" class="form-control form-control-user" id="email" name="email" onChange="this.value=this.value.toLowerCase()" placeholder="INFORME SEU E-MAIL" required>
                                         </div>
                                         <div class="form-group">
-                                            <input type="text" class="form-control cpfOuCnpj" id="cpf" name="cpf" placeholder="INFORME SEU CPF" required>
+                                            <input type="text" class="form-control form-control-user cpfOuCnpj" id="cpf" name="cpf" placeholder="INFORME SEU CPF" required>
                                         </div>
                                         <!--
                                         <div class="form-group">
