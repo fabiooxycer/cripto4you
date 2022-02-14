a6b28ce62fa4831f7edc819f32867ea4422d94fb
fabiooxycer <fabiovieira@oxycer.com>
2022-02-14 10:24:31 -0300

    Update menu.php

--- broker/includes/menu.php
+++ broker/includes/menu.php
@@ -6,45 +6,46 @@
 </li>
 <hr class="sidebar-divider">
 
-    <div class="sidebar-heading">
-        Broker
+<div class="sidebar-heading">
+    Broker
+</div>
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseMeuInvestimento" aria-expanded="true" aria-controls="collapseMeuInvestimento">
+        <i class="fas fa-fw fa-coins"></i>
+        <span>Meu Investimento</span>
+    </a>
+    <div id="collapseMeuInvestimento" class="collapse" aria-labelledby="headingMeuInvestimento" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="meu-investimento">Meu Investimento</a>
+        </div>
     </div>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseMeuInvestimento" aria-expanded="true" aria-controls="collapseMeuInvestimento">
-            <i class="fas fa-fw fa-coins"></i>
-            <span>Meu Investimento</span>
-        </a>
-        <div id="collapseMeuInvestimento" class="collapse" aria-labelledby="headingMeuInvestimento" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item" href="meu-investimento">Meu Investimento</a>
-            </div>
+</li>
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseAppsUser" aria-expanded="true" aria-controls="collapseAppsUser">
+        <i class="fas fa-fw fa-users"></i>
+        <span>Relatório</span>
+    </a>
+    <div id="collapseAppsUser" class="collapse" aria-labelledby="headingAppsUser" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="usuarios-cadastros">Cadastros</a>
         </div>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseAppsUser" aria-expanded="true" aria-controls="collapseAppsUser">
-            <i class="fas fa-fw fa-users"></i>
-            <span>Relatório</span>
-        </a>
-        <div id="collapseAppsUser" class="collapse" aria-labelledby="headingAppsUser" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item" href="usuarios-cadastros">Cadastros</a>
-            </div>
-        </div>
-    </li>
+    </div>
+</li>
+<hr class="sidebar-divider">
 
-    <div class="sidebar-heading">
-        Atendimento
-    </div>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSuporte" aria-expanded="true" aria-controls="collapseSuporte">
-            <i class="fas fa-fw fa-life-ring"></i>
-            <span>Suporte</span>
-        </a>
-        <div id="collapseSuporte" class="collapse" aria-labelledby="headingSuporte" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item" href="fila-atendimento">Fila de Atendimento</a>
-            </div>
+<div class="sidebar-heading">
+    Atendimento
+</div>
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSuporte" aria-expanded="true" aria-controls="collapseSuporte">
+        <i class="fas fa-fw fa-life-ring"></i>
+        <span>Suporte</span>
+    </a>
+    <div id="collapseSuporte" class="collapse" aria-labelledby="headingSuporte" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="fila-atendimento">Fila de Atendimento</a>
         </div>
+    </div>
 </li>
 
 <hr class="sidebar-divider d-none d-md-block">
@@ -54,11 +55,11 @@
         <span>Configurações Site</span>
     </a>
     <div id="collapseConfigSite" class="collapse" aria-labelledby="headingConfigSite" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">            
+        <div class="bg-white py-2 collapse-inner rounded">
             <a class="collapse-item" href="contato">Contato</a>
             <a class="collapse-item" href="noticias">Notícias</a>
             <a class="collapse-item" href="notificacoes">Notificações</a>
-            <a class="collapse-item" href="seo">SEO</a>            
+            <a class="collapse-item" href="seo">SEO</a>
         </div>
     </div>
 </li>
