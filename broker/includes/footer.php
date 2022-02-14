b2fbc796d738b4014f250d366af89d604ccab41f
fabiooxycer <fabiovieira@oxycer.com>
2022-02-14 16:14:30 -0300

    update

--- broker/includes/footer.php
+++ broker/includes/footer.php
@@ -33,20 +33,28 @@
 </div>
 <!-- /EXIBE MODAL DE LOGOFF -->
 
-<script src="<?= $siteurl ?>/assets/vendor/jquery/jquery.min.js"></script>
-<script src="<?= $siteurl ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?= $siteurl ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-<script src="<?= $siteurl ?>/assets/js/sb-admin-2.min.js"></script>
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="assets/js/sb-admin-2.min.js"></script>
 <!--
-<script src="<?= $siteurl ?>/assets/vendor/chart.js/Chart.min.js"></script>
-<script src="<?= $siteurl ?>/assets/js/demo/chart-area-demo.js"></script>
-<script src="<?= $siteurl ?>/assets/js/demo/chart-pie-demo.js"></script>
+<script src="assets/vendor/chart.js/Chart.min.js"></script>
+<script src="assets/js/demo/chart-area-demo.js"></script>
+<script src="assets/js/demo/chart-pie-demo.js"></script>
 -->
-<script src="<?= $siteurl ?>/assets/vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="<?= $siteurl ?>/assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
-<script src="<?= $siteurl ?>/assets/js/demo/datatables-demo.js"></script>
+<script src="assets/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="assets/js/demo/datatables-demo.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+<!-- WYSIWYG Editor js -->
+<script src="assets/wysiwyag/jquery.richtext.js"></script>
+<script src="assets/wysiwyag/richText1.js"></script>
+
+<!--Summernote js-->
+<script src="assets/summernote/summernote-bs4.js"></script>
+<script src="assets/summernote2.js"></script>
 
 </body>
 
--- broker/includes/header.php
+++ broker/includes/header.php
@@ -4,7 +4,7 @@ include("database.php");
 $pdo = BancoCadastros::conectar();
 include("selects-db.php");
 require('phpmailer/hdw-phpmailer.php');
-  
+
 ?>
 
 <!DOCTYPE html>
@@ -15,13 +15,13 @@ require('phpmailer/hdw-phpmailer.php');
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="<?php echo $seo['descricao'];?>">
+  <meta name="description" content="<?php echo $seo['descricao']; ?>">
   <meta name="author" content="Cripto4You">
-  <title><?php echo $seo['titulo'];?></title>
-  <link href="/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <title><?php echo $seo['titulo']; ?></title>
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <link href="/assets/css/sb-admin-2.min.css" rel="stylesheet">
-  <link href="/assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
   <!-- Mascaras -->
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
   <script src="https://igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js"></script>
@@ -30,21 +30,29 @@ require('phpmailer/hdw-phpmailer.php');
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-  
+
+  <!-- WYSIWYG Editor css -->
+  <link href="assets/wysiwyag/richtext.css" rel="stylesheet" />
+
+  <!--Summernote css-->
+  <link rel="stylesheet" href="assets/summernote/summernote-bs4.css">
+
   <style>
-      .faturamento{
-          line-height: 35px;
-      }
-      .faturamento.hide{
-          display: inline-block;
-          background-color: #f1f1f1;
-          line-height: 99999999px;
-          height: 27px;
-          overflow: hidden;
-      }
-      .botao-faturamento{
-        cursor: pointer;
-      }
+    .faturamento {
+      line-height: 35px;
+    }
+
+    .faturamento.hide {
+      display: inline-block;
+      background-color: #f1f1f1;
+      line-height: 99999999px;
+      height: 27px;
+      overflow: hidden;
+    }
+
+    .botao-faturamento {
+      cursor: pointer;
+    }
   </style>
 </head>
 
@@ -55,7 +63,7 @@ require('phpmailer/hdw-phpmailer.php');
   <div id="wrapper">
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard">
-        <div class="sidebar-brand-text mx-3"><img src="assets/img/logo.png" width="100%" alt="<?php echo $seo['titulo'];?>"></div>
+        <div class="sidebar-brand-text mx-3"><img src="assets/img/logo.png" width="100%" alt="<?php echo $seo['titulo']; ?>"></div>
       </a>
 
       <?php
