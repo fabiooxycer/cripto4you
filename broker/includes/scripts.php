0a7ba4ea0b02c3a3ca48a49c714dfd37e52ebcc9
fabiooxycer <fabiovieira@oxycer.com>
2022-02-14 17:11:37 -0300

    Update scripts.php

--- broker/includes/scripts.php
+++ broker/includes/scripts.php
@@ -56,17 +56,6 @@
 </script>
 
 <script>
-    var options = {
-        onKeyPress: function(cpf, ev, el, op) {
-            var masks = ['000.000.000-000', '00.000.000/0000-00'];
-            $('.cpfOuCnpj').mask((cpf.length > 14) ? masks[1] : masks[0], op);
-        }
-    }
-
-    $('.cpfOuCnpj').length > 11 ? $('.cpfOuCnpj').mask('00.000.000/0000-00', options) : $('.cpfOuCnpj').mask('000.000.000-00#', options);
-</script>
-
-<script>
     function verifica(value) {
         var input = document.getElementById("chave_pix");
 
