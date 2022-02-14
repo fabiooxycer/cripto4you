9ad93aafd22e8b1ba084f93b7b23671ccccc8106
fabiooxycer <fabiovieira@oxycer.com>
2022-02-14 10:23:04 -0300

    Update senha.php

--- broker/senha.php
+++ broker/senha.php
@@ -17,7 +17,7 @@ if (empty($email) || empty($cpf)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Digital Intelligentia">
+    <meta name="author" content="Cripto4You">
     <title>Plataforma de Gestão Broker | Cripto4You</title>
     <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
