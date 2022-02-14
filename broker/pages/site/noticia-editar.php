e23eff2368016a9362523205845c60f2d4893c96
fabiooxycer <fabiovieira@oxycer.com>
2022-02-14 17:52:26 -0300

    update

--- broker/pages/site/noticia-editar.php
+++ broker/pages/site/noticia-editar.php
@@ -1,7 +1,7 @@
 <?php
 if (!isset($_SESSION)) session_start();
 
-$nivel = 98;
+$nivel = 1;
 
 if (!isset($_SESSION['UsuarioID']) or ($_SESSION['UsuarioNivel'] < $nivel)) {
     echo "<script>alert('VOCÊ NÃO POSSUI PERMISSÃO PARA EXIBIR ESTÁ TELA!');location.href='entrar';</script>";
