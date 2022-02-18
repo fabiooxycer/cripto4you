36efa813a72fe43f5a66be0d714f0e626b567b98
fabiooxycer <fabiovieira@oxycer.com>
2022-02-18 09:44:21 -0300

    Update meu-investimento.php

--- broker/pages/broker/meu-investimento.php
+++ broker/pages/broker/meu-investimento.php
@@ -110,125 +110,28 @@ include('../../includes/header.php');
         </div>
     </div>
 
-    <!-- Exibe o Modal para inserção dos Cliente -->
-    <div class="modal" id="modalNovoUsuario" tabindex="-1" role="dialog" aria-hidden="true">
+    <!-- Exibe o Modal para solicitação de saque -->
+    <div class="modal" id="modalSaque" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-xl " role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">ADICIONAR NOVO CLIENTE/USUÁRIO</h4>
+                    <h4 class="modal-title">SOLICITAR SAQUE</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
                 </div>
                 <div class="modal-body">
-                    <form action="clientes" method="post" enctype="multipart/form-data">
+                    <form action="meu-investimento" method="post" enctype="multipart/form-data">
                         <div class="form-body">
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group">
-                                        <label for="basicInput">Nome</label>
-                                        <input type="text" class="form-control" id="nome" name="nome" onChange="this.value=this.value.toUpperCase()" autocomplete="off" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="basicInput">RG</label>
-                                        <input type="text" class="form-control" id="rg" name="rg" autocomplete="off" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="basicInput">CPF</label>
-                                        <input type="text" class="form-control" id="cpf" name="cpf" onkeyup="cpfCheck(this)" maxlength="18" onkeydown="javascript: fMasc( this, mCPF );" autocomplete="off" required><span id="cpfResponse"></span></p>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="basicInput">Telefone</label>
-                                        <input type="text" class="form-control phone" id="telefone" name="telefone" autocomplete="off" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-9">
-                                    <div class="form-group">
-                                        <label for="basicInput">E-mail</label>
-                                        <input type="email" class="form-control" id="email" name="email" onChange="this.value=this.value.toLowerCase()" autocomplete="off" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label for="basicInput">CEP</label>
-                                        <input type="text" class="form-control" id="cep" name="cep" onchange="pesquisacep(this.value);" autocomplete="off" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-9">
-                                    <div class="form-group">
-                                        <label for="basicInput">Endereço</label>
-                                        <input type="text" class="form-control" id="endereco" name="endereco" onChange="this.value=this.value.toUpperCase()" autocomplete="off" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label for="basicInput">Número</label>
-                                        <input type="text" class="form-control" id="numero" name="numero" autocomplete="off" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label for="basicInput">Complemento</label>
-                                        <input type="text" class="form-control" id="complemento" name="complemento" onChange="this.value=this.value.toUpperCase()" autocomplete="off">
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label for="basicInput">Bairro</label>
-                                        <input type="text" class="form-control" id="bairro" name="bairro" onChange="this.value=this.value.toUpperCase()" autocomplete="off" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label for="basicInput">Cidade</label>
-                                        <input type="text" class="form-control" id="cidade" name="cidade" onChange="this.value=this.value.toUpperCase()" autocomplete="off" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label for="basicInput">Estado</label>
-                                        <input type="text" class="form-control" id="estado" name="estado" onChange="this.value=this.value.toUpperCase()" autocomplete="off" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label for="basicInput">PIX</label>
-                                        <select type="text" class="form-control" id="tipo_pix" name="tipo_pix" autocomplete="off" onchange="verifica(this.value)" required>
-                                            <option value="">Selecione...</option>
-                                            <option value="Chave Aleatória">Chave Aleatória</option>
-                                            <option value="E-mail">E-mail</option>
-                                            <option value="CNPJ">CNPJ</option>
-                                            <option value="CPF">CPF</option>
-                                            <option value="Telefone">Telefone</option>
-                                            <option value="Não Possuo">Não Possuo</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="basicInput">Chave PIX</label>
-                                        <input type="text" class="form-control" id="chave" name="chave" autocomplete="off">
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label for="basicInput">Nível Acesso</label>
-                                        <select type="text" class="form-control" id="nivel" name="nivel" autocomplete="off" required>
-                                            <option value="">Selecione...</option>
-                                            <option value="1">Cliente</option>
-                                            <option value="99">Operador</option>
-                                            <option value="100">Administrador</option>
-                                        </select>
+                                        <label for="basicInput">Preencha o valor que deseja sacar:</label>
+                                        <input type="text" class="form-control" id="valor" name="valor" onKeyPress="return(moeda(this,'.',',',event))" onChange="this.value=this.value.toUpperCase()" autocomplete="off" required>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="form-actions">
-                            <button type="submit" name="adicionar" class="btn btn-primary"><i class="fa fa-check"></i> CADASTRAR</button>
+                            <button type="submit" name="solicitar" class="btn btn-primary"><i class="fa fa-check"></i> SOLICITAR SAQUE</button>
                             <button type="button" class="btn btn-secondary text-white" data-dismiss="modal"><i class="fa fa-times-circle"></i> FECHAR</button>
                         </div>
                     </form>
@@ -253,136 +156,37 @@ function get_post_action($name)
 }
 
 // Verifica qual botao foi clicado
-switch (get_post_action('desativar', 'ativar', 'adicionar')) {
+switch (get_post_action('solicitar')) {
 
-    case 'desativar':
+    case 'solicitar':
 
         if (!empty($_POST)) {
 
-            $id          = $_POST['id'];
-            $status_down = '2';
-
-            //Validaçao dos campos:
-            $validacao = true;
+            $usuario     = $_SESSION['UsuarioID'];
+            $descricao   = 'Saque aporte/lucro';
+            $tipo        = '2';
+            $valor       = $_POST['valor'];
+            $comprovante = '-';
+            $dt_criacao  = date("Y-m-d");
+            $hr_criacao  = date("H:i:s");
+            $confirmado  = '2';
         }
-
-        //Delete do banco:
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = 'UPDATE tbl_usuarios SET status = ? WHERE id = ?';
+        $sql = "INSERT INTO tbl_investimentos (id_usuario, descricao, tipo, valor, comprovante, dt_criacao, hr_criacao, confirmado) VALUES(?,?,?,?,?,?,?,?)";
         $q = $pdo->prepare($sql);
-        $q->execute(array($status_down, $id));
+        $q->execute(array($id_usuario, $descricao, $tipo, $valor, $comprovante, $dt_criacao, $hr_criacao, $confirmado));
         echo '<script>setTimeout(function () { 
             swal({
               title: "Parabéns!",
-              text: "Cliente/Usuário desativado com sucesso!",
+              text: "Solicitação de saque realizada com sucesso!",
               type: "success",
               confirmButtonText: "OK" 
             },
             function(isConfirm){
               if (isConfirm) {
-                window.location.href = "clientes";
+                window.location.href = "meu-investimento";
               }
             }); }, 1000);</script>';
-        break;
-
-    case 'ativar':
-
-        if (!empty($_POST)) {
-
-            $id          = $_POST['id'];
-            $status_up = '1';
-
-            //Validaçao dos campos:
-            $validacao = true;
-        }
-
-        //Delete do banco:
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = 'UPDATE tbl_usuarios SET status = ? WHERE id = ?';
-        $q = $pdo->prepare($sql);
-        $q->execute(array($status_up, $id));
-        echo '<script>setTimeout(function () { 
-                swal({
-                  title: "Parabéns!",
-                  text: "Cliente/Usuário ativado com sucesso!",
-                  type: "success",
-                  confirmButtonText: "OK" 
-                },
-                function(isConfirm){
-                  if (isConfirm) {
-                    window.location.href = "clientes";
-                  }
-                }); }, 1000);</script>';
-        break;
-
-    case 'adicionar':
-
-        if (!empty($_POST)) {
-
-            $nome        = $_POST['nome'];
-            $rg          = $_POST['rg'];
-            $cpf         = $_POST['cpf'];
-            $telefone    = $_POST['telefone'];
-            $email       = $_POST['email'];
-            $cep         = $_POST['cep'];
-            $endereco    = $_POST['endereco'];
-            $numero      = $_POST['numero'];
-            $complemento = $_POST['complemento'];
-            $bairro      = $_POST['bairro'];
-            $cidade      = $_POST['cidade'];
-            $estado      = $_POST['estado'];
-            $tipo_pix    = $_POST['tipo_pix'];
-            $chave       = $_POST['chave'];
-            $status      = '1';
-            $nivel       = $_POST['nivel'];
-            $dt_cadastro = date("Y-m-d");
-
-            if ($complemento == '') {
-                $complemento = '-';
-            }
-            if ($chave == '') {
-                $chave = '-';
-            }
-        }
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = 'SELECT * FROM tbl_usuarios WHERE cpf = "' . $_POST['cpf'] . '"';
-        $q = $pdo->prepare($sql);
-        $q->execute(array($_POST['cpf']));
-        $data = $q->fetch(PDO::FETCH_ASSOC);
-
-        if ($data['cpf'] != $_POST['cpf']) {
-
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "INSERT INTO tbl_usuarios (nome, rg, cpf, telefone, email, cep, endereco, numero, complemento, bairro, cidade, estado, tipo_pix, chave, status, nivel, dt_cadastro) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-            $q = $pdo->prepare($sql);
-            $q->execute(array($nome, $rg, $cpf, $telefone, $email, $cep, $endereco, $numero, $complemento, $bairro, $cidade, $estado, $tipo_pix, $chave, $status, $nivel, $dt_cadastro));
-            echo '<script>setTimeout(function () { 
-            swal({
-              title: "Parabéns!",
-              text: "Cliente/Usuário cadastrado com sucesso!",
-              type: "success",
-              confirmButtonText: "OK" 
-            },
-            function(isConfirm){
-              if (isConfirm) {
-                window.location.href = "clientes";
-              }
-            }); }, 1000);</script>';
-        }
-        if ($data['cpf'] == $_POST['cpf']) {
-            echo '<script>setTimeout(function () { 
-                swal({
-                  title: "Atenção!",
-                  text: "Cliente/Usuário já possui cadastro!",
-                  type: "warning",
-                  confirmButtonText: "OK" 
-                },
-                function(isConfirm){
-                  if (isConfirm) {
-                    window.location.href = "clientes";
-                  }
-                }); }, 1000);</script>';
-        }
         break;
 
     default:
