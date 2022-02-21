e102b7948881f46c98b8b9436fab29ffe942555f
fabiooxycer <fabiovieira@oxycer.com>
2022-02-21 16:02:50 -0300

    Update meu-investimento.php

--- broker/pages/broker/meu-investimento.php
+++ broker/pages/broker/meu-investimento.php
@@ -3,7 +3,7 @@
 if ($_SERVER['HTTP_HOST'] != 'localhost') {
     if (!isset($_SESSION)) session_start();
 
-    $nivel = 98;
+    $nivel = 1;
 
     if (!isset($_SESSION['UsuarioID']) or ($_SESSION['UsuarioNivel'] < $nivel)) {
         echo "<script>alert('VOCÊ NÃO POSSUI PERMISSÃO PARA EXIBIR ESTÁ TELA!');location.href='entrar';</script>";
