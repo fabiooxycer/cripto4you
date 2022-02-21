c29e899403d868432ff02c2d46bc7aa049670c7c
fabiooxycer <fabiovieira@oxycer.com>
2022-02-21 09:52:22 -0300

    Update sobre.php

--- sobre.php
+++ sobre.php
@@ -202,7 +202,7 @@ include('includes/header.php');
           <div class="consultation--desc">
             Solicite nossa Consultoria
           </div>
-          <form class="mb-0" action="includes/email-contato.php" method="post">
+          <form class="mb-0" action="sobre" method="post">
             <div class="row">
               <div class="col-xs-12 col-sm-12 col-md-4">
                 <input type="text" class="form-control" name="nome" id="nome" placeholder="Seu Nome" required>
@@ -268,3 +268,47 @@ include('includes/header.php');
 
 
 <?php include('includes/footer.php'); ?>
+
+<?php
+// Chama função para pegar o POST de cada FORM
+function get_post_action($name)
+{
+    $params = func_get_args();
+
+    foreach ($params as $name) {
+        if (isset($_POST[$name])) {
+            return $name;
+        }
+    }
+}
+
+// Verifica qual botao foi clicado
+switch (get_post_action('contato')) {
+
+    case 'contato':
+
+        if (!empty($_POST)) {
+
+            $nome     = $_POST['nome'];
+            $email    = $_POST['email'];
+            $telefone = $_POST['telefone'];
+            $mensagem = $_POST['mensagem'];
+        }
+
+        // ENVIA TELEGRAM    
+        $apiToken = "5155649072:AAF466dIaOiGvEb9qCGavLXNHVXE06ZRPwo";
+        $data2 = [
+            "chat_id" => "-1001709220235", // ID Canal Contato Site
+            'parse_mode' => 'HTML',
+            'text' => "\n<b>CONTATO PELO SITE</b> \n\nNome: $nome\nE-mail: $email\nTelefone: $telefone\nMensagem: $Mensagem\n",
+        ];
+
+        $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data2));
+
+        echo "<script>alert('SUA MENSAGEM FOI ENVIADA COM SUCESSO!');location.href='inicio';</script>";
+
+        break;
+
+    default:
+}
+?>
