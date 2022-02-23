c6d19a0abb209d025c2c6d8fb3585a98dec6bcdb
fabiooxycer <fabiovieira@oxycer.com>
2022-02-23 09:45:25 -0300

    Update menu.php

--- broker/includes/menu.php
+++ broker/includes/menu.php
@@ -12,25 +12,26 @@
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
 </li>
-<hr class="sidebar-divider">
 
 <?php if ($_SESSION['UsuarioNivel'] >= '1') { ?>
+    <hr class="sidebar-divider">
     <div class="sidebar-heading">
         Broker
     </div>
     <?php if ($_SESSION['UsuarioNivel'] != '100') { ?>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseMeuInvestimento" aria-expanded="true" aria-controls="collapseMeuInvestimento">
-            <i class="fas fa-fw fa-coins"></i>
-            <span>Investimento</span>
-        </a>
-        <div id="collapseMeuInvestimento" class="collapse" aria-labelledby="headingMeuInvestimento" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item" href="meu-investimento">Extrato</a>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseMeuInvestimento" aria-expanded="true" aria-controls="collapseMeuInvestimento">
+                <i class="fas fa-fw fa-coins"></i>
+                <span>Investimento</span>
+            </a>
+            <div id="collapseMeuInvestimento" class="collapse" aria-labelledby="headingMeuInvestimento" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="meu-investimento">Extrato</a>
+                </div>
             </div>
-        </div>
-    </li>
-    <?php } if ($_SESSION['UsuarioNivel'] >= '98') { ?>
+        </li>
+    <?php }
+    if ($_SESSION['UsuarioNivel'] >= '98') { ?>
         <hr class="sidebar-divider">
 
         <div class="sidebar-heading">
