b36a07217b9338fa399e46b14573e442d3dbcf34
fabiooxycer <fabiovieira@oxycer.com>
2022-03-02 17:20:34 -0300

    Update

--- broker/dashboard.php
+++ broker/dashboard.php
@@ -58,4 +58,85 @@ include('includes/header.php');
     </div>
 </div>
 
+<?php if ($_SESSION['UsuarioContrato'] == 1) { ?>
+    <script type="text/javascript">
+        $(window).on('load', function() {
+            $('#modalContrato').modal('show');
+        });
+    </script>
+
+    <!-- Modal Contrato -->
+    <div class="modal fade" id="modalContrato" tabindex="-1" role="dialog" aria-labelledby="modalContrato" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" ole="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">ACEITE DE CONTRATO</h5>
+                </div>
+                <form action="includes/footer.php" method="post">
+                    <div class="modal-body">
+                        <div style="width: 100%; height:550px; overflow-y:scroll;">
+                            <br>
+                            <?php include('includes/contrato.php'); ?>
+                            <br>
+                            <br>
+                        </div>
+                    </div>
+                    <div class="modal-footer text-center">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" data-dismiss="modal"><i class="fa fa-times-circle"></i> FECHAR</button>
+                        <button type="submit" name="contrato" class="btn btn-sm btn-outline-success"><i class="fa fa-check"></i> CONCORDO</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+<?php
+    // Chama função para pegar o POST de cada FORM
+    function get_post_action($name)
+    {
+        $params = func_get_args();
+
+        foreach ($params as $name) {
+            if (isset($_POST[$name])) {
+                return $name;
+            }
+        }
+    }
+
+    // Verifica qual botao foi clicado
+    switch (get_post_action('contrato')) {
+
+        case 'contrato':
+
+            if (!empty($_POST)) {
+
+                $id_usuario       = $_SESSION['UsuarioID'];
+                $contrato_aceito  = 2;
+            }
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = 'UPDATE tbl_usuarios SET contrato_aceito = ? WHERE id = ?';
+            $q = $pdo->prepare($sql);
+            $q->execute(array($contrato_aceito, $id_transacao));
+
+            echo '<script>setTimeout(function () { 
+                    swal({
+                      title: "Parabéns!",
+                      text: "Contrato aceito com sucesso!",
+                      type: "success",
+                      confirmButtonText: "OK" 
+                    },
+                    function(isConfirm){
+                      if (isConfirm) {
+                        window.location.href = "dashboard";
+                      }
+                    }); }, 1000);</script>';
+
+            break;
+
+        default:
+    }
+}
+?>
+
 <?php include('includes/footer.php'); ?>
