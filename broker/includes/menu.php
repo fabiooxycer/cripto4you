df0be0d275ac260549aa5cb63f392d2cec92f3de
fabiooxycer <fabiovieira@oxycer.com>
2022-03-02 14:56:06 -0300

    Update menu.php

--- broker/includes/menu.php
+++ broker/includes/menu.php
@@ -67,9 +67,10 @@
             </div>
         </div>
     </li>
+    <hr class="sidebar-divider d-none d-md-block">
 <?php } ?>
 
-<hr class="sidebar-divider d-none d-md-block">
+
 <div class="text-center d-none d-md-inline">
     <button class="rounded-circle border-0" id="sidebarToggle"></button>
 </div>
