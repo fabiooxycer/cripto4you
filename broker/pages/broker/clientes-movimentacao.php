e2cbd0a46d330fa87c57fb36db31d461a6e0efdb
fabiooxycer <fabiovieira@oxycer.com>
2022-03-02 10:51:09 -0300

    Update clientes-movimentacao.php

--- broker/pages/broker/clientes-movimentacao.php
+++ broker/pages/broker/clientes-movimentacao.php
@@ -276,7 +276,7 @@ $data = $q->fetch(PDO::FETCH_ASSOC);
                                     <div class="form-group">
                                         <label for="basicInput">Valor:</label>
                                         <input type="hidden" class="form-control" id="id" name="id" value="<?php echo $data['id']; ?>" autocomplete="off" readonly>
-                                        <input type="text" class="form-control" id="valor" name="valor" onKeyPress="return(moeda(this,'.',',',event))" placeholder="Informe o valor do lucro" onChange="this.value=this.value.toUpperCase()"  autocomplete="off" required>
+                                        <input type="text" class="form-control" id="valor" name="valor" onKeyPress="return(moeda(this,'.',',',event))" placeholder="Informe o valor do lucro" onChange="this.value=this.value.toUpperCase()" autocomplete="off" required>
                                     </div>
                                 </div>
                             </div>
@@ -317,16 +317,17 @@ switch (get_post_action('saque', 'deposito', 'lucro', 'liberar', 'cancelar', 're
 
         if (!empty($_POST)) {
 
-            $usuario        = $_POST['id'];
-            $descricao      = 'Saque aporte/lucro';
-            $tipo           = '2';
-            $valor         = $_POST['valor'];
-            $valor_saque    = str_replace(',', '.', str_replace('.', '', $_POST['valor']));
+            $usuario          = $_POST['id'];
+            $descricao        = 'Saque aporte/lucro';
+            $tipo             = '2';
+            $valor            = $_POST['valor'];
+            $valor_saque      = str_replace(',', '.', str_replace('.', '', $_POST['valor']));
             $valor_solicitado = number_format($valor_saque, 2, ',', '.');
-            $valor1 = str_replace('.', '', $valor_solicitado);
-            $valor2 = str_replace(',00', '', $valor1);
-            $comprovante    = '-';
-            $confirmado     = '2';
+            $valor1           = str_replace('.', '', $valor_solicitado);
+            $valor2           = str_replace(',00', '', $valor1);
+            $comprovante      = '-';
+            $confirmado       = '2';
+            $prox_saque       = $_POST['prox_saque'];
 
             $dt_criacao = date("Y-m-d");
             $hr_criacao = date("H:i:s");
@@ -336,9 +337,8 @@ switch (get_post_action('saque', 'deposito', 'lucro', 'liberar', 'cancelar', 're
             $hr_saque = date('H:i:s', $timestamp2);
         }
 
-        if ($data['tipo_contrato'] == 3)
 
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql1 = 'SELECT sum(valor) FROM tbl_investimentos WHERE id_usuario = "' . $usuario . '" AND tipo = 3 AND confirmado = 1';
         foreach ($pdo->query($sql1) as $data_lucro) {
             $lucro = $data_lucro['sum(valor)'];
@@ -375,6 +375,10 @@ switch (get_post_action('saque', 'deposito', 'lucro', 'liberar', 'cancelar', 're
         if ($saldo_cliente >= $valor2) {
 
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql_saque = "UPDATE tbl_usuarios set dt_saque = ? WHERE id = ?";
+            $q = $pdo->prepare($sql_saque);
+            $q->execute(array($prox_saque, $usuario));
+
             $sql = "INSERT INTO tbl_investimentos (id_usuario, descricao, tipo, valor, comprovante, dt_criacao, hr_criacao, confirmado, operador) VALUES(?,?,?,?,?,?,?,?,?)";
             $q = $pdo->prepare($sql);
             $q->execute(array($usuario, $descricao, $tipo, $valor_saque, $comprovante, $dt_criacao, $hr_criacao, $confirmado, $_SESSION['UsuarioNome']));
