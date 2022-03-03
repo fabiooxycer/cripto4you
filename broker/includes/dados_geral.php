7819b6b5f4e85a4a832ce2d679b9b693fb432dfd
fabiooxycer <fabiovieira@oxycer.com>
2022-03-02 21:10:25 -0300

    Update

--- broker/includes/dados_geral.php
+++ broker/includes/dados_geral.php
@@ -1,5 +1,5 @@
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h4 mb-0 text-gray-800">TOTAL DE MOVIMENTAÇÕES CRIPTO4YOU</h1>
+    <h1 class="h4 mb-0 text-gray-800">DASHBOARD ADMIN</h1>
 </div>
 <div class="row">
     <div class="col-xl-4 col-md-6 mb-4">
