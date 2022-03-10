829a167bff03ca9bec23c0db9fc590393b06bd25
fabiooxycer <fabiovieira@oxycer.com>
2022-03-10 10:12:49 -0300

    Remove App Antigo

--- broker/pages/broker/teste.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$valor       = '1.000,00';
-$valor_trade = str_replace(',', '.', str_replace('.', '', $valor));
-$valor_fim   = str_replace('.00', '', $valor_trade);
-$percentual  = '10';
-$taxa        = ($valor_fim / 100) * $percentual;
-echo $taxa;
