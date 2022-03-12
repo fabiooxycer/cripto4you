f9c61b1bf66ad244fbe63097077da3e0203a4c9d
fabiooxycer <fabiovieira@oxycer.com>
2022-03-12 07:21:38 -0300

    Update

--- broker/includes/topnavbar.php
+++ broker/includes/topnavbar.php
@@ -14,67 +14,9 @@
                     </a>
                 </div>
             </div>
-            <div class="iq-search-bar" style="width: 100%;">
-                <!-- TradingView Widget BEGIN -->
-                <div class="tradingview-widget-container">
-                    <div class="tradingview-widget-container__widget"></div>
-                    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-                        {
-                            "symbols": [{
-                                    "description": "",
-                                    "proName": "BINANCE:BTCUSDT"
-                                },
-                                {
-                                    "description": "",
-                                    "proName": "BINANCE:ETHUSDT"
-                                },
-                                {
-                                    "description": "",
-                                    "proName": "BINANCE:LTCUSDT"
-                                },
-                                {
-                                    "description": "",
-                                    "proName": "BINANCE:BNBUSDT"
-                                },
-                                {
-                                    "description": "",
-                                    "proName": "BINANCE:ADAUSDT"
-                                },
-                                {
-                                    "description": "",
-                                    "proName": "BINANCE:NEARUSDT"
-                                },
-                                {
-                                    "description": "",
-                                    "proName": "BINANCE:MANAUSDT"
-                                },
-                                {
-                                    "description": "",
-                                    "proName": "BINANCE:SANDUSDT"
-                                },
-                                {
-                                    "description": "",
-                                    "proName": "BINANCE:GALAUSDT"
-                                },
-                                {
-                                    "description": "",
-                                    "proName": "XRPUSDT"
-                                },
-                                {
-                                    "description": "",
-                                    "proName": "BINANCE:BUSDUSDT"
-                                }
-                            ],
-                            "showSymbolLogo": true,
-                            "colorTheme": "dark",
-                            "isTransparent": true,
-                            "displayMode": "compact",
-                            "locale": "br"
-                        }
-                    </script>
-                </div>
-                <!-- TradingView Widget END -->
-            </div>
+            <!-- <div class="iq-search-bar" style="width: 100%;">
+                
+            </div> -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation">
                 <i class="ri-menu-3-line"></i>
             </button>
